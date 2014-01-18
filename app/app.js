95f07db73cf231f310e654f5157a4b4c400362a7
Andreas McDermott <graytest@gmail.com>
2014-01-19 00:04:56 +0100

    Added event

--- app/app.js
+++ app/app.js
@@ -2,9 +2,33 @@
 
 .controller('AppCtrl', function ($scope) {
   $scope.tooltipIsVisible = false;
+  $scope.tooltipPos = {x:0, y:0};
   
-  $scope.toogleTooltip = function (item) {
-    $(item).css('top:' + $(item).css('top')).css('left', $(item).css('left'));
+  $scope.showTooltip = function (x, y) {
+    $scope.tooltipPos = {x, y};
     $scope.tooltipIsVisible = true;
+  };
+  
+  $scope.hideTooltip = function () {
+    $scope.tooltipIsVisible = false;
+  };
+})
+
+.directive('service', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      type: '=type'
+    },
+    template: '<i class="fa fa-{{ type }}"></i>',
+    link: function (scope, element, attrs) {
+      element.bind('mouseenter', function (e) {
+        scope.showTooltip(e.clientX, e.clientY);
+      });
+      
+      element.bind('mouseleave', function () {
+        scope.hideTooltip();
+      });
+    }
   };
 });
