903b63a066bf02a48c11e49d8bc211a527cef0ca
Andreas McDermott <graytest@gmail.com>
2014-01-19 19:53:18 +0100

    Pos

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
   $('td > i').hover(function () {
-    $('.tooltip-arrow').css('left', $(this).position().left + 10);
+    $('.tooltip-arrow').css('left', $(this).position().left + $(this).width() / 2);
     $('.tooltip').show();
   }, function () {
     $('.tooltip').hide();
