673f45cb71370dc277da08475f8a56375fa169b2
Andreas McDermott <graytest@gmail.com>
2014-01-19 12:53:39 +0100

    Getting event to work

--- app/app.js
+++ app/app.js
@@ -3,28 +3,19 @@
 .controller('AppCtrl', function ($scope) {
   $scope.tooltipIsVisible = false;
   $scope.tooltipPos = {left:0, top:0};
-  
-  $scope.showTooltip = function (x, y) {
-    alert(x + ", " + y);
-    $scope.tooltipPos = {left: x, top: y};
-    $scope.tooltipIsVisible = true;
-  };
-  
-  $scope.hideTooltip = function () {
-    $scope.tooltipIsVisible = false;
-  };
 })
 
-.directive('gmService', function () {
+.directive('gmService', function () { 
   return {
     link: function (scope, element, attrs) {
       $(element).bind('mouseenter', function () {
-        var pos = $(this).position()
-        scope.showTooltip(pos.left, pos.top);
+        var pos = 
+        scope.tooltipPos = $(this).position();
+        scope.tooltipIsVisible = true;
       });
       
       $(element).bind('mouseleave', function () {
-        scope.hideTooltip();
+        scope.tooltipIsVisible = false;
       });
     }
   };
