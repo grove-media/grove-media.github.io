424e2c4edff88030dbe2f65abc7d15e4195a1a61
Andreas McDermott <graytest@gmail.com>
2014-01-19 19:50:06 +0100

    jquery instead

--- app/app.js
+++ app/app.js
@@ -1,24 +1,8 @@
-﻿angular.module('app', ['ngRoute'])
-
-.controller('AppCtrl', function ($scope) {
-  $scope.gmServices = [
-    { css: 'fa-code' },
-    { css: 'fa-cloud' },
-    { css: 'fa-mobile' },
-    { css: 'fa-bar-chart-o' },
-    { css: 'fa-pencil' },
-    { css: 'fa-wrench' }
-  ];
-  
-  $scope.activeService = null;
-  $scope.tooltipPos = { left: 0 };
-  
-  $scope.setActiveService = function (srvc, e) {
-    $scope.activeService = srvc;
-    $scope.tooltipPos = { left: e.offsetX };
-  };
-  
-  $scope.shouldShowTooltip = function () {
-    return $scope.activeService !== null;
-  };
+﻿$(function () {
+  $('td > i').hover(function () {
+    $('.tooltip-arrow').css('left', $(this).position().left + 10);
+    $('.tooltip').show();
+  }, function () {
+    $('.tooltip').hide();
+  });
 });
