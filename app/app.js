fff54617dc9bc6ae9edf5d0bdd03d63a06fae7dc
Andreas McDermott <graytest@gmail.com>
2014-01-20 23:04:21 +0100

    Effects

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
   $('td').hover(function () {
     var $i = $(this).children('i');
-    $('.tooltip-arrow').animate({'left: ' + ($i.position().left + $i.width() / 2 + 'px')});
+    $('.tooltip-arrow').animate({left:  ($i.position().left + $i.width() / 2)});
     $('.tooltip-content > div').hide();
     $($(this).attr('data-display')).show();
     $('.tooltip').fadeIn('fast');
